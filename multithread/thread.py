6497b6925684bbd0022d7317b2c3b56b62510916
Tony Gionfriddo <tony.gionfriddo@charter.com>
2020-08-10 17:00:32 -0600

    join two threads

--- multithread/thread.py
+++ multithread/thread.py
@@ -13,5 +13,9 @@ def my_func(name):
 if __name__ == "__main__":
     print("thread_1 started\n")
     t = threading.Thread(target=my_func, args=["my_arg"])
+    # daemon=True to run thread in background will be killed when main thread is finished if work is not completed
+    # t = threading.Thread(target=my_func, args=["my_arg"], daemon=True)
     t.start()
+    # joins main thread with second thread and will wait until second thread completes before completing main thread.
+    t.join()
     print("thread_1 ended\n")

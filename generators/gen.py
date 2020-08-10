62dbaa0a76f2bee4fa84ca7353d581538f19b095
Tony Gionfriddo <tony.gionfriddo@charter.com>
2020-08-10 14:50:26 -0600

    generator functions send, close, throw

--- generators/gen.py
+++ generators/gen.py
@@ -38,10 +38,21 @@ print(next(finite)) # 3 END!
 import sys
 
 num_list = [1,2,3]
-lc = [num**2 for num in range(20)]
+lc = [num**2 for num in range(10)]
 print(lc)
-print(sys.getsizeof(lc)) # output in bytes
+print(sys.getsizeof(lc))  # output in bytes
 
-gc = (num**2 for num in range(20))
+gc = (num**2 for num in range(10))
 print(gc)
-print(sys.getsizeof(gc)) # output in bytes
+print(sys.getsizeof(gc))  # output in bytes
+
+print('modify generator values as its iterated')
+for i in gc:
+    print(i)
+    modify = i**2
+    print(modify)
+    if modify > 200:
+        infinite.throw(ValueError("Modified value exeeds 200"))  # throw exception on current gen value
+                                                                 # or modified gen value
+    infinite.send(modify)  # modifies the current iteration of the generator value
+

653fa8f3443e66124110732de32bd9f8a6261ade
Tony Gionfriddo <tony.gionfriddo@charter.com>
2020-08-10 13:43:37 -0600

    list comprehensions and generator comprehension

--- generators/gen.py
+++ generators/gen.py
@@ -6,3 +6,39 @@ def infinite_seq():
     while True:
         yield num
         num += 1
+        yield "This is the second yield statement"
+
+
+infinite = infinite_seq()
+
+print(type(infinite))
+print(next(infinite))
+print(next(infinite))
+print(next(infinite))
+print(next(infinite))
+
+
+# StopIteration error if list is finite, and generator reaches the end of the list
+
+def finite_seq():
+    num = [1,2,3]
+    for n in num:
+        yield n
+
+finite = finite_seq()
+
+print(type(finite))
+print(next(finite)) # 1
+print(next(finite)) # 2
+print(next(finite)) # 3 END!
+#print(next(finite)) # StopIteration
+
+
+# Generator comprehensions use parenthesis instead of square brackets like list comprehensions
+
+num_list = [1,2,3]
+lc = [num**2 for num in num_list]
+print(lc)
+
+gc = (num**2 for num in num_list)
+print(gc)

a9c1affaaa3d9221d073107ed7553c9dc644539f
Tony Gionfriddo <tony.gionfriddo@charter.com>
2020-08-10 16:51:02 -0600

    two threads

--- multithread/thread.py
+++ multithread/thread.py
@@ -2,4 +2,16 @@
 # os - schedules work for the cpu
 # process - program as it is being executed
 # thread - part of a process
+import time
+import threading
 
+def my_func(name):
+    print(f"thread_2 started with {name}\n")
+    time.sleep(10)
+    print("thread_2 ended\n")
+
+if __name__ == "__main__":
+    print("thread_1 started\n")
+    t = threading.Thread(target=my_func, args=["my_arg"])
+    t.start()
+    print("thread_1 ended\n")

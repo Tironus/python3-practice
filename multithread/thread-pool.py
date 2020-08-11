5f386dee3afd5b46b5beaa3d20d4c65cdbe4aa33
Tony Gionfriddo <tony.gionfriddo@charter.com>
2020-08-10 18:57:09 -0600

    updated with actual thread name

--- multithread/thread-pool.py
+++ multithread/thread-pool.py
@@ -5,13 +5,14 @@
 import time
 import random
 import concurrent.futures
+import threading
 
 number_of_threads = 0
 
 def my_func(name):
     global number_of_threads
     number_of_threads += 1
-    thread_id = number_of_threads
+    thread_id = threading.current_thread().ident
     print(f"thread_{thread_id} started with {name}\n")
     time.sleep(random.randrange(3, 7, 2))
     print(f"thread_{thread_id} ended\n")
@@ -19,7 +20,7 @@ def my_func(name):
 
 if __name__ == "__main__":
     number_of_threads += 1
-    print(f"thread_{number_of_threads} started\n")
+    print(f"thread_{threading.current_thread().ident} started\n")
     with concurrent.futures.ThreadPoolExecutor(max_workers=3) as e:
         e.map(my_func, ['arg_1', 'arg_2', 'arg_3'])
-    print(f"thread_{number_of_threads} ended\n")
+    print(f"thread_{threading.current_thread().ident} ended\n")

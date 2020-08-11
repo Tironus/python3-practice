755919df87641cb21120ee6201c9384441416988
Tony Gionfriddo <tony.gionfriddo@charter.com>
2020-08-11 13:45:41 -0600

    fixed consumer_producer pipeline with thread locks

--- multithread/producer_consumer_pipeline.py
+++ multithread/producer_consumer_pipeline.py
@@ -1,20 +1,30 @@
 import time
 import concurrent.futures
 import random
+import threading
 
 LAST_MSG = 'LAST_MSG'
+
 
 class Pipeline():
     def __init__(self, capacity):
         self.capacity = capacity
         self.message = None
+        self.producer_lock = threading.Lock()
+        self.consumer_lock = threading.Lock()
+        self.consumer_lock.acquire()
 
     def set_message(self, message):
+        self.producer_lock.acquire()
         self.message = message
+        self.consumer_lock.release()
 
     def get_message(self):
+        self.producer_lock.release()
         message = self.message
+        self.consumer_lock.acquire()
         return message
+
 
 def producer(pipeline):
     for _ in range(pipeline.capacity):
@@ -23,6 +33,7 @@ def producer(pipeline):
         producer_pipeline.append(message)
         print(f'producing message: {message}')
     pipeline.set_message('LAST_MSG')
+
 
 def consumer(pipeline):
     while pipeline.message is not LAST_MSG:

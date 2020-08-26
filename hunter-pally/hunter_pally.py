d682af5eaecbf60ea4307c347983ec8de9fd82a6
Tony Gionfriddo <tony.gionfriddo@charter.com>
2020-08-26 00:18:23 -0600

    its working!!

--- hunter-pally/hunter_pally.py
+++ hunter-pally/hunter_pally.py
@@ -1,19 +1,65 @@
 import pyautogui
 from PIL import ImageGrab
+import random
+import time
+
+random_timings = [0.4, 0.53, 0.64, 0.75]
 
 try:
     while True:
-        # combat starts
-        combat = ImageGrab.grab(bbox=(1250, 675, 1251, 676))
-        if combat.getpixel((0,0)) == (255,255,255,255):
+        s = time.perf_counter()
+        # combat starts 1
+        combat = ImageGrab.grab(bbox=(1250,675,3250,677))
+        if combat.getpixel((1,0)) == (255,255,255,255):
             # begin combat
             print('FIGHT!')
-            inquisition = ImageGrab.grab(bbox=(1315,675,1316,676))
-            if inquisition.getpixel((0,0)) == (255,255,255,255):
-                print('inquisition running')
+
+            # wrath 224
+            if combat.getpixel((224,0)) == (255,255,255,255):
+                print('cast avenging wrath')
+                pyautogui.hotkey('7')
+
+            # reckoning 392
+            if combat.getpixel((392,0)) == (255,255,255,255):
+                print('cast reckoning')
+                pyautogui.hotkey('6')
+
+            # selfless healer 309
+            if combat.getpixel((309,0)) == (255,255,255,255):
+                print('cast selfless healer')
+                pyautogui.hotkey('5')
+
+            # hand of hinderance 471
+            if combat.getpixel((471,0)) == (255,255,255,255):
+                print('cast hand of hinderance')
+                pyautogui.hotkey('ctrl', '6')
+
+            # holy power 135
+            if combat.getpixel((142,0)) == (255,255,255,255):
+
+                # inquisition 63
+                if combat.getpixel((63,0)) == (255, 255, 255, 255):
+                    print('inquisition running')
+                else:
+                    print('cast inquisition')
+                    pyautogui.hotkey('2')
+                print('cast templars verdict')
+                pyautogui.hotkey('3')
+                pyautogui.hotkey('ctrl', '2')
             else:
-                print('cast inquisition')
-                pyautogui.hotkey('2')
+                if combat.getpixel((63,0)) == (255, 255, 255, 255):
+                    print('inquisition running')
+                else:
+                    print('cast inquisition')
+                    pyautogui.hotkey('2')
+                print('light grant me power!')
+                pyautogui.hotkey('3')
+                pyautogui.hotkey('ctrl', '4')
+                pyautogui.hotkey('ctrl', '5')
+                pyautogui.hotkey('4')
+                pyautogui.hotkey('1')
+            e = time.perf_counter() - s
+            print(f'time elapsed: {e}')
         else:
             # no combat
             print('looking for prey...')
--- hunter-pally/mouse_locator.py
+++ hunter-pally/mouse_locator.py
@@ -1,6 +1,11 @@
 import pyautogui
+from PIL import ImageGrab
+from PIL import Image
 
 while True:
     x, y = pyautogui.position()
     print(f'x: {x}, y: {y}')
-    print(pyautogui.pixel(x, y))
+
+# x: 620 y: 340 - combat
+
+# x: 489 y: 1002

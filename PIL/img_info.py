c66ef95bf431793db6c1f2ba3fbe563b7f357b02
Tony Gionfriddo <tony.gionfriddo@charter.com>
2020-09-02 11:59:10 -0600

    working, needs priority added

--- PIL/img_info.py
+++ PIL/img_info.py
@@ -2,32 +2,70 @@ from PIL import ImageGrab
 import time
 from PIL import Image
 import pyautogui
+import keyboard
+import concurrent.futures
+import threading
+import queue
 
 
-def check_heal(img, zip_loc):
-    px = zip_loc[0][0]
-    py = zip_loc[0][1]
-    mx = zip_loc[1][0]
-    my = zip_loc[1][1]
+class Pipeline(queue.Queue):
+    def __init__(self):
+        super().__init__(maxsize=10)
+
+    def set_message(self, message):
+        self.put(message)
+
+    def get_message(self):
+        message = self.get()
+        return message
+
+
+def pally_actions(pipeline):
+    coords = pipeline.get_message()
+    print(f'consuming msg {coords[0], coords[1]}')
+
+    mx = coords[0]
+    my = coords[1]
+    if mx is None or my is None:
+        return
+
+    s = time.perf_counter()
+    pyautogui.moveTo(mx, my)
+    pyautogui.click()
+    pyautogui.hotkey('ctrl', '4')
+    pyautogui.hotkey('4')
+    pyautogui.hotkey('2')
+    pyautogui.hotkey('ctrl', '6')
+    pyautogui.hotkey('5')
+    pyautogui.hotkey('9')
+    pyautogui.hotkey('7')
+    pyautogui.hotkey('0')
+    pyautogui.hotkey('ctrl', '7')
+    # pyautogui.hotkey('ctrl', '5')
+    e = time.perf_counter() - s
+    print(f'elapsed ability time: {e}')
+
+
+def check_heal(img, zip_loc, pipeline):
+    px = zip_loc[0]
+    py = zip_loc[1]
+    mx = zip_loc[2]
+    my = zip_loc[3]
 
     if img.getpixel((px, py)) == (234, 51, 35, 255):
-        pyautogui.moveTo(mx, my)
-        pyautogui.click()
-        pyautogui.hotkey('ctrl', '4')
-        pyautogui.press('4')
-        pyautogui.press('2')
-        pyautogui.hotkey('ctrl', '6')
-        pyautogui.press('5')
-        pyautogui.press('9')
-        pyautogui.press('7')
-        pyautogui.press('0')
-        pyautogui.hotkey('ctrl', '7')
-        #pyautogui.hotkey('ctrl', '5')
+        print('heal now!')
+        print(f'setting msg to {mx}, {my}')
+        pipeline.set_message([mx, my])
     else:
-        print('no heal needed')
+        print('setting msg to None, None')
+        pipeline.set_message([None, None])
 
-#time.sleep(4)
+
+#time.sleep(0.2)
 while True:
+    s = time.perf_counter()
+
+    #print('pulled image')
     p = ImageGrab.grab(bbox=(175, 522, 680, 1028))
     pix = p.load()
 
@@ -49,9 +87,26 @@ while True:
 
     zip_loc = zip(dung_px, dung_mouse_loc)
 
-    for z in zip_loc:
-        check_heal(p, z)
+    pipeline = Pipeline()
+    pipeline1 = Pipeline()
+    pipeline2 = Pipeline()
+    pipeline3 = Pipeline()
+    pipeline4 = Pipeline()
 
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as e:
+        e.submit(check_heal, p, [10, 10, 119, 292], pipeline)
+        e.submit(pally_actions, pipeline)
+        e.submit(check_heal, p, [110, 10, 169, 292], pipeline1)
+        e.submit(pally_actions, pipeline1)
+        e.submit(check_heal, p, [210, 10, 219, 292], pipeline2)
+        e.submit(pally_actions, pipeline2)
+        e.submit(check_heal, p, [310, 10, 269, 292], pipeline3)
+        e.submit(pally_actions, pipeline3)
+        e.submit(check_heal, p, [410, 10, 319, 292], pipeline4)
+        e.submit(pally_actions, pipeline4)
+
+    e = time.perf_counter() - s
+    print(f'elapsed: {e}')
 
 
 
@@ -101,9 +156,9 @@ while True:
 #pix[410,310] = (0,0,0,255)
 
 # row 5
-pix[10,410] = (0,0,0,255)
-pix[110,410] = (0,0,0,255)
-pix[210,410] = (0,0,0,255)
-pix[310,410] = (0,0,0,255)
-pix[410,410] = (0,0,0,255)
+#pix[10,410] = (0,0,0,255)
+#pix[110,410] = (0,0,0,255)
+#pix[210,410] = (0,0,0,255)
+#pix[310,410] = (0,0,0,255)
+#pix[410,410] = (0,0,0,255)
 #img.save('edited_v1.png')

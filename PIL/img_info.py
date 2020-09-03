9349673cae340cd49d0d1902ddbe9549fb1418dc
Tony Gionfriddo <tony.gionfriddo@charter.com>
2020-09-03 07:39:13 -0600

    consolidated pipeline processing to single consumer

--- PIL/img_info.py
+++ PIL/img_info.py
@@ -13,55 +13,60 @@ class Pipeline(queue.Queue):
         super().__init__(maxsize=10)
 
     def set_message(self, message):
-        self.put(message)
+        self.put(message, block=False)
 
     def get_message(self):
-        message = self.get()
+        message = self.get(block=False)
         return message
+
+    def get_qsize(self):
+        return self.qsize()
 
 
 def pally_actions(pipeline):
-    coords = pipeline.get_message()
-    print(f'consuming msg {coords[0], coords[1]}')
+    start = time.perf_counter()
+    print(f'queue size: {pipeline.get_qsize()}')
+    for _ in range(pipeline.get_qsize()):
+        coords = pipeline.get_message()
+        print(f'consuming msg {coords[0], coords[1]}')
 
-    mx = coords[0]
-    my = coords[1]
-    if mx is None or my is None:
-        return
+        s = time.perf_counter()
+        pyautogui.moveTo(coords[0], coords[1])
+        pyautogui.click()
+        keyboard.press_and_release('4')
+        keyboard.press_and_release('ctrl', '4')
+        keyboard.press_and_release('2')
+        keyboard.press_and_release('ctrl', '6')
+        #keyboard.press_and_release('5')
+        keyboard.press_and_release('9')
+        keyboard.press_and_release('7')
+        keyboard.press_and_release('0')
+        keyboard.press_and_release('ctrl', '7')
+        pyautogui.hotkey('ctrl', '5')
+        e = time.perf_counter() - s
+        print(f'elapsed ability time: {e}')
+        time.sleep(0.2)
+        elapsed = time.perf_counter() - start
 
-    s = time.perf_counter()
-    pyautogui.moveTo(mx, my)
-    pyautogui.click()
-    pyautogui.hotkey('ctrl', '4')
-    pyautogui.hotkey('4')
-    pyautogui.hotkey('2')
-    pyautogui.hotkey('ctrl', '6')
-    pyautogui.hotkey('5')
-    pyautogui.hotkey('9')
-    pyautogui.hotkey('7')
-    pyautogui.hotkey('0')
-    pyautogui.hotkey('ctrl', '7')
-    # pyautogui.hotkey('ctrl', '5')
-    e = time.perf_counter() - s
-    print(f'elapsed ability time: {e}')
+        if elapsed > 1.3:
+            return
 
 
 def check_heal(img, zip_loc, pipeline):
-    px = zip_loc[0]
-    py = zip_loc[1]
-    mx = zip_loc[2]
-    my = zip_loc[3]
+    px = zip_loc[0][0]
+    py = zip_loc[0][1]
+    mx = zip_loc[1][0]
+    my = zip_loc[1][1]
 
     if img.getpixel((px, py)) == (234, 51, 35, 255):
         print('heal now!')
         print(f'setting msg to {mx}, {my}')
         pipeline.set_message([mx, my])
     else:
-        print('setting msg to None, None')
-        pipeline.set_message([None, None])
+        print('no healing needed')
+        pass
 
 
-#time.sleep(0.2)
 while True:
     s = time.perf_counter()
 
@@ -85,26 +90,20 @@ while True:
 
     dung_mouse_loc = [[119, 292], [169, 292], [219, 292], [269, 292], [319, 292]]
 
-    zip_loc = zip(dung_px, dung_mouse_loc)
+    zip_loc = zip(raid_px, raid_mouse_loc)
 
     pipeline = Pipeline()
-    pipeline1 = Pipeline()
-    pipeline2 = Pipeline()
-    pipeline3 = Pipeline()
-    pipeline4 = Pipeline()
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as e:
-        e.submit(check_heal, p, [10, 10, 119, 292], pipeline)
-        e.submit(pally_actions, pipeline)
-        e.submit(check_heal, p, [110, 10, 169, 292], pipeline1)
-        e.submit(pally_actions, pipeline1)
-        e.submit(check_heal, p, [210, 10, 219, 292], pipeline2)
-        e.submit(pally_actions, pipeline2)
-        e.submit(check_heal, p, [310, 10, 269, 292], pipeline3)
-        e.submit(pally_actions, pipeline3)
-        e.submit(check_heal, p, [410, 10, 319, 292], pipeline4)
-        e.submit(pally_actions, pipeline4)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=30) as e:
+        for z in zip_loc:
+            e.submit(check_heal, p, z, pipeline)
+        #e.submit(check_heal, p, [110, 10, 169, 292], pipeline)
+        #e.submit(check_heal, p, [210, 10, 219, 292], pipeline)
+        #e.submit(check_heal, p, [310, 10, 269, 292], pipeline)
+        #e.submit(check_heal, p, [410, 10, 319, 292], pipeline)
 
+    pally_actions(pipeline)
+    print(f'queue size: {pipeline.get_qsize()}')
     e = time.perf_counter() - s
     print(f'elapsed: {e}')
 
